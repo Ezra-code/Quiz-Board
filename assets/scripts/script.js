228066bb97d0617116c29d7fb64b2b63c92cefe1
KN-cod <ezra.njeru@student.moringaschool.com>
2022-05-14 22:22:23 +0300

    add the required files for the project

---
+++ assets/scripts/script.js
@@ -0,0 +1,93 @@
+// let startBtn = document.getElementById('start-btn')
+// let contEl = document.getElementById('cont-el')
+// let submitEl = document.getElementById('submit')
+// let scoreEl = document.getElementById('score')
+// let choiceEl = document.getElementsByName('choice')
+// let choiceTwo = document.getElementsByName('choice2')
+// let choiceThree = document.getElementsByName('choice3')
+// let message = document.getElementById('message-el')
+// let result = document.getElementById('result')
+// let percentEl = document.querySelector("#percent-el")
+// let score = [];
+// let sum = 0
+
+// startBtn.addEventListener('click', function(){
+//     startBtn.classList.add('hide')
+//     contEl.classList.remove('hide')
+// })
+
+// submitEl.addEventListener('click', function(){
+//     contEl.classList.add('hide')
+//     scoreEl.classList.remove('hide')
+//     scoreQuiz()
+//     Message()
+// })
+// function scoreQuiz(){
+//     let percent;
+//     for(i=0; i<choiceEl.length; i++){
+//         if(choiceEl[i].checked){
+//             let choice = choiceEl[i].value 
+//             if(choice == 'correct'){
+//                 score.push(1)
+//                 score
+//             }else{
+//                 score.push(0)
+//                 score            
+//             }
+//         }
+//     }
+
+//     for(i=0; i<choiceTwo.length; i++){
+//         if(choiceTwo[i].checked){
+//             let choice = choiceTwo[i].value 
+//             if(choice == 'correct'){
+//                 score.push(1)
+//                 score
+//             }else{
+//                 score.push(0)
+//                 score
+//             }
+//         }
+//     }
+
+//     for(i=0; i<choiceThree.length; i++){
+//         if(choiceThree[i].checked){
+//             let choice = choiceThree[i].value 
+//             if(choice == 'correct'){
+//                 score.push(1)
+//                 score
+//             }else{
+//                 score.push(0)
+//                 score
+                
+//             }
+//         }
+//     }
+
+//     for(i=0; i<score.length; i++){
+//         sum += score[i]
+//         sum
+//         result.innerHTML = sum
+//     }
+//     percent = Math.round((sum * 100) / 3)
+//     percent
+//     percentEl.innerHTML = percent + "%"
+// }
+
+// function Message(){
+//     switch(sum){
+//         case 1:
+//             message.innerHTML = "Below average"
+//         break;
+
+//         case 2:
+//             message.innerHTML = "Good perfomance"
+//         break;
+//         case 3:
+//             message.innerHTML = "Excellent perfomance"
+//         break;
+//         default:
+//             message.innerHTML = "sorry"
+//     }
+    
+// }
